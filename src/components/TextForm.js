4c240fe19446d39e762a5b48053c6c44815b07d7
Tanishka-Garg <tan27peace@gmail.com>
2023-03-24 15:57:24 +0530

    packages

---
+++ src/components/TextForm.js
@@ -0,0 +1,61 @@
+import React, {useState}  from 'react'
+
+export default function TextForm(props) {
+  const handleUpOnClick = () => {
+    setText(text.toUpperCase())
+  }
+  const handleLowOnClick = () => {
+    setText(text.toLowerCase())
+  }
+  const clearOnClick = () => {
+    setText("")
+  }
+  const copyOnClick = () => {
+    let copyText = text;
+    navigator.clipboard.writeText(copyText);
+    alert("Copied to clipboard")
+    
+  }
+  
+  const handleExtraSpaces = () => {
+    let copyText = text.split(/[ ]+/)
+    setText(copyText.join(" "))
+  }
+  const handleOnChange = (event) => {
+    setText(event.target.value)
+  }
+  const wordCount = (str) => {
+    if (str === '') {
+      return 0;
+    }
+    else {
+      return str.trim().split(/\s+/).length;
+    }
+  }
+  const [text, setText] = useState("");
+
+  return (
+    <>
+    
+      <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleOnChange} id="Textarea"
+            rows="7" style={{backgroundColor: props.mode==='light'?'white':'#121212', color: props.mode==='light'?'black':'whiten' }}></textarea>
+      </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpOnClick}>to uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowOnClick}>to lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>remove extra spaces</button>
+        <div>
+          <button className="btn btn-primary mx-2 my-2" onClick={clearOnClick}>clear</button>
+          <button className="btn btn-primary mx-2 my-2" onClick={copyOnClick}>copy</button>
+        </div>
+      </div>
+      <div className="container my-2 mx-1" style={{color: props.mode==='light'?'black':'white'}}>
+        <h2>Text Summary</h2>
+        <p>{wordCount(text)} words  {text.length} characters</p>
+        <p> read time: { Math.round(0.006*text.split(" ").length)} minutes</p>
+      </div>
+    </>
+  )
+}

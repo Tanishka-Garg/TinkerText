7236487fbd95b7b83c2ea1b4e01f0dddb0603899
Tanishka-Garg <tan27peace@gmail.com>
2023-03-24 17:10:06 +0530

    Updates

---
+++ precache-manifest.50e0568d0d05c4de31e784806f0ac54d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "615e32de4e175a6ae4d9",
+    "url": "/TinkerText/static/css/main.55c1680f.chunk.css"
+  },
+  {
+    "revision": "615e32de4e175a6ae4d9",
+    "url": "/TinkerText/static/js/main.615e32de.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/TinkerText/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "7401d9e35929effbace8",
+    "url": "/TinkerText/static/js/2.7401d9e3.chunk.js"
+  },
+  {
+    "revision": "9cc69545080af17405e7",
+    "url": "/TinkerText/static/js/runtime~main.9cc69545.js"
+  },
+  {
+    "revision": "169d5b3b473123e50936f47da8c43dd3",
+    "url": "/TinkerText/index.html"
+  }
+];
